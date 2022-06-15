ccf1e24cad1173a3f8a9276ab7e16d55426ece4b
Anibal Montero <anibal.montero.p@gmail.com>
2022-06-15 12:34:10 +0200

    Plantillas ejs con navbar

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use(require('./routes/routes'));
 require('./databases/mongo');
