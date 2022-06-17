1f7a72e0049bca1e149d38e45724f2b7a36a7c08
bogdaniam <mihai.bogdan.mardale@gmail.com>
2022-06-17 20:58:13 +0200

    creado modelo coche, insertado en controlers.usuarios el apartado para insertar datos en la tabla coches, validacion datos registro

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -1,22 +1,57 @@
 const Factura = require('../models/Factura');
 const Usuario = require('../models/Usuario');
+const Coche = require('../models/Coche');
+
+
 
 const usuarios = {
   registro: (req, res) => {
     res.render('../views/pages/registro');
   },
   registrodone: async (req, res) => {
-    const anibal = await Usuario.create({
-      nombre: req.body.nombreRegistro,
-      apellido: 'Perdomo',
-      dni: '0423456Y',
-      telefono: '1234567',
-      email: 'kindg@loco.com',
-    });
-    console.log(anibal.toJSON());
-    // nombre = req.body.nombreRegistro;
-    // res.send(nombre);
-    // sequelize.sync();
+
+
+      var dni=false
+      var numero = req.body.dniRegistro.slice(0, req.body.dniRegistro.length - 1);
+      var letra_dni = req.body.dniRegistro[8].toUpperCase();
+      var resto = numero % 23;
+      var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+      //var encontrado = letras[resto];
+      if (letras[resto] == letra_dni) {
+        dni=true
+      }//34567765F
+        
+    if ((req.body.nombreRegistro.match(/^[a-z ,.'-]+$/i))
+      && (req.body.apellidosRegistro.match(/^[a-z ,.'-]+$/i))
+      && (req.body.dniRegistro.match(/^\d{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/))
+      && (req.body.telefonoRegistro.match(/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/))
+      && (req.body.mailRegistro.match(/^[a-zA-Z0-9_\-\.~]{2,}@[a-zA-Z0-9_\-\.~]{2,}\.[a-zA-Z]{2,4}$/))
+      && dni) {
+      const anibal = await Usuario.create({
+        nombre: req.body.nombreRegistro,
+        apellido: req.body.apellidosRegistro,
+        dni: req.body.dniRegistro,
+        telefono: req.body.telefonoRegistro,
+        email: req.body.mailRegistro,
+      });
+      console.log(anibal.toJSON().user_id);
+      const car = await Coche.create({
+        matricula: req.body.matriculaRegistro,
+        marca: req.body.marcaRegistro,
+        modelo: req.body.modeloRegistro,
+        fecha: req.body.fechaRegistro,
+        fk_user_id: anibal.toJSON().user_id,
+      });
+      console.log(anibal.toJSON());
+      console.log(car.toJSON());
+      // nombre = req.body.nombreRegistro;
+      // res.send(nombre);
+      // sequelize.sync();
+    } else {
+      console.log("Datos invalidos")
+    }
+
+
   },
   factura: (request, response) => {
     Factura.find({}).then((factura) => {
