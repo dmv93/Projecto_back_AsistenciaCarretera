76ab2a4875362dbff22ccaa721d3b3642ebc7403
Anibal Montero <anibal.montero.p@gmail.com>
2022-06-18 19:09:36 +0200

    Inicio de autenticacion

---
+++ server/controllers/auth.js
@@ -0,0 +1,25 @@
+const { compare } = require('../helpers/handleBcrypt');
+const Usuario = require('../models/Usuario');
+
+const auth = {
+  loginCtrl: async (req, res) => {
+    // debemos encerrarlos en un bloque try catch
+    const { usuarioLogin, passwordLogin } = req.body;
+
+    const usuario = await Usuario.findOne({
+      where: { nombre: usuarioLogin },
+    });
+
+    if (!usuario) {
+      res.status(404);
+      res.send({ error: 'usuario no encontrado' });
+    }
+    const checkPassword = await compare(passwordLogin, usuario.contrasena);
+    if (checkPassword) {
+      console.log('contraseña correcta');
+    } else {
+      console.log('contraseña incorrecta');
+    }
+  },
+};
+module.exports = auth;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,14 +5,16 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const pagina = require('../controllers/paginas');
 const usuarios = require('../controllers/usuarios');
 const staff = require('../controllers/staff');
+const auth = require('../controllers/auth');
 
 const router = express.Router();
 
 router.get('/', pagina.home);
 router.get('/login', pagina.login);
+router.post('/login', urlencodedParser, auth.loginCtrl);
 router.get('/nosotros', pagina.nosotros);
 router.get('/contacto', pagina.contacto);
-router.get('/coche', pagina.coche)
+router.get('/coche', pagina.coche);
 router.get('/solicitud', pagina.solicitud);
 router.get('/registro', usuarios.registro);
 router.post('/registro', urlencodedParser, usuarios.registrodone);
