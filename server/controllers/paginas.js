a4adae5855749c90feab22e0a8cd9ab5e6485666
bogdaniam <mihai.bogdan.mardale@gmail.com>
2022-06-22 17:47:28 +0200

    comprobar datos

--- server/controllers/paginas.js
+++ server/controllers/paginas.js
@@ -28,8 +28,9 @@ const paginas = {
   tarifas: (req, res) => {
     res.render('../views/pages/tarifas.pug');
   },
-  mapa: (req, res) => {
-    res.render('../views/pages/mapa');
+  mapa: async (req, res) => {
+    let guardarDatos = await req.cookies.nombre;
+    res.render('../views/pages/mapa', {datosGuardados: guardarDatos});
   },
 };
 module.exports = paginas;
